6bc815c7e2b522242cdb982db00ae268ce278b6c
karn5276 <karnpable5777@gmail.com>
2026-05-05 14:36:32 +0000

    added broadcast join over customer table in gold layer

--- DLT_Pipelines/auto_loader_customer_order_pipeline/transformations/gold/customer_order.py
+++ DLT_Pipelines/auto_loader_customer_order_pipeline/transformations/gold/customer_order.py
@@ -1,20 +1,33 @@
 from pyspark import pipelines as dp
 from pyspark.sql.functions import col, to_timestamp
+from pyspark.sql import functions as F
+from pyspark.sql.functions import broadcast
 
 # =====================================================
 # 6. GOLD LAYER (JOIN CUSTOMERS + ORDERS)
 # =====================================================
 @dp.materialized_view(name="gold_customer_orders")
 def gold_customer_orders():
-    return spark.sql("""
-        SELECT 
-            c.customer_id,
-            c.name,
-            COUNT(o.order_id) AS total_orders,
-            SUM(o.amount) AS total_spent,
-            MAX(o.order_time) AS last_order_time
-        FROM customer_orders.autoloader_demo.silver_customers_scd2 c
-        LEFT JOIN customer_orders.autoloader_demo.silver_orders_scd1 o
-        ON c.customer_id = o.customer_id
-        GROUP BY c.customer_id, c.name
-    """)
+    customers = spark.table("customer_orders.autoloader_demo.silver_customers_scd2")
+    orders = spark.table("customer_orders.autoloader_demo.silver_orders_scd1")
+
+ # perform broadcast join on customers table
+    result = broadcast(customers).alias("c") \
+        .join(
+            orders.alias("o"),
+            F.col("c.customer_id") == F.col("o.customer_id"),
+            "left"
+        ) \
+        .groupBy(
+            F.col("c.customer_id"),
+            F.col("c.name")
+        ) \
+        .agg(
+            F.count("o.order_id").alias("total_orders"),
+            F.sum("o.amount").alias("total_spent"),
+            F.max("o.order_time").alias("last_order_time")
+        )
+
+    return result
+
+
